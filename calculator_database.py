5d10f46688a2125c85ab1ce0330cf66a70b81bc6
yash <yash29112000@gmail.com>
2022-08-09 19:19:05 +0530

    second commit

--- calculator_database.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(host = 'localhost', username = 'root', password = 'abcde')
-
-cursor = db.cursor()
-
-cursor.execute("CREATE DATABASE calculator")
--- drp.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(host = "localhost", username = "root", password = "abcde", database = "calculator")
-
-cursor = db.cursor()
-
-seq = "DROP TABLE addition, substraction, multiplication, divide, modulus"
-
-cursor.execute(seq)
